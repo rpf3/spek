cf45dd3dd3c2294efcc60584752f871e4a560806
Dickie Field <me@rpf3.io>
2022-08-13 17:10:21 -0400

    feat: Use the table component in the dev route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Column } from '$lib/types';
+
+	import Table from '$lib/components/Table.svelte';
+
+	const columns: Column[] = [
+		{
+			id: 'name',
+			name: 'Name'
+		},
+		{
+			id: 'email',
+			name: 'Email'
+		},
+		{
+			id: 'age',
+			name: 'Age'
+		}
+	];
+</script>
+
+<Table {columns} />

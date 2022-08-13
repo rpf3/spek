6877bf6a13dddd305146e03d0d4e063a2a913efc
Dickie Field <me@rpf3.io>
2022-08-13 17:09:51 -0400

    feat: Create a table row wrapper component

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import type { Column } from '$lib/types';
 
+	import Row from './Row.svelte';
+
 	export let columns: Column[];
 </script>
 
 <div class="flex">
-	<div class="flex flex-row gap-2">
-		{#each columns as column}
-			<div>{column.name}</div>
-		{/each}
-	</div>
+	<Row>
+		<svelte:fragment>
+			{#each columns as column}
+				<div>{column.name}</div>
+			{/each}
+		</svelte:fragment>
+	</Row>
 </div>

a897f536e62640ad19bfcfb0a273601ff6af7698
Dickie Field <me@rpf3.io>
2022-08-13 17:01:14 -0400

    feat: Display the table header row

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Column } from '$lib/types';
+
+	export let columns: Column[];
+</script>
+
+<div class="flex">
+	<div class="flex flex-row gap-2">
+		{#each columns as column}
+			<div>{column.name}</div>
+		{/each}
+	</div>
+</div>

65794c8cae6f5b54dce8f3f5c476430ed2ed1a26
Dickie Field <me@rpf3.io>
2022-08-14 17:38:34 -0400

    refactor: Move data row creation to its own util

---
+++ src/lib/utils/data.ts
@@ -0,0 +1,45 @@
+import type { DataItem } from '$lib/types';
+import type { DataRow } from '$lib/types';
+
+function sanitizeValue(value: unknown): string {
+	if (typeof value === 'string') {
+		return value;
+	}
+
+	return JSON.stringify(value);
+}
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+function createDataItems(data: any): DataItem[] {
+	const keys = Object.keys(data);
+
+	const items = keys.map((key) => {
+		const value = sanitizeValue(data[key]);
+
+		const item: DataItem = {
+			key: key,
+			value: value
+		};
+
+		return item;
+	});
+
+	return items;
+}
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+function createDataRows(data: any[]): DataRow[] {
+	const rows = data.map((item) => {
+		const items = createDataItems(item);
+
+		const row: DataRow = {
+			items: items
+		};
+
+		return row;
+	});
+
+	return rows;
+}
+
+export { createDataRows };

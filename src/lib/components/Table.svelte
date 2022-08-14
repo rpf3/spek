4d2f176fd133129ef79cbff8d2c9a4f1efbf09f7
Dickie Field <me@rpf3.io>
2022-08-14 08:06:50 -0400

    feat: Create a table cell wrapper component
    This commit creates a table cell wrapper that uses CSS flex box styles
    to set the same width for each cell in a column.

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
 	import type { Column } from '$lib/types';
 
+	import Cell from './Cell.svelte';
 	import Row from './Row.svelte';
 
 	export let columns: Column[];
 </script>
 
-<div class="flex">
+<div class="flex flex-col">
 	<Row>
 		<svelte:fragment>
 			{#each columns as column}
-				<div>{column.name}</div>
+				<Cell>
+					{column.name}
+				</Cell>
 			{/each}
 		</svelte:fragment>
 	</Row>

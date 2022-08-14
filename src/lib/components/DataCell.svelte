fd23cc5a30e928362fd039d312c20190885e19b8
Dickie Field <me@rpf3.io>
2022-08-14 11:20:17 -0400

    refactor: Move the row value function

--- src/lib/components/DataCell.svelte
+++ src/lib/components/DataCell.svelte
@@ -1,24 +1,15 @@
 <script lang="ts">
 	import type { Column } from '$lib/types';
-	import type { DataItem } from '$lib/types';
 	import type { DataRow } from '$lib/types';
+
+	import { getRowValue } from '$lib/utils';
 
 	import Cell from './Cell.svelte';
 
 	export let column: Column;
 	export let row: DataRow;
-
-	function getDataItem(row: DataRow, key: string): DataItem | null {
-		const result = row.items.find((item) => item.key === key);
-
-		if (result === undefined) {
-			return null;
-		}
-
-		return result;
-	}
 </script>
 
 <Cell>
-	{getDataItem(row, column.key)?.value}
+	{getRowValue(row, column.key)}
 </Cell>

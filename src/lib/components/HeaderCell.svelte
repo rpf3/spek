5f9e53d553ae91729c9aa297bb268128f20ab217
Dickie Field <me@rpf3.io>
2022-08-14 11:02:46 -0400

    feat: Dispatch a sort event from header cells

--- src/lib/components/HeaderCell.svelte
+++ src/lib/components/HeaderCell.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import type { Column } from '$lib/types';
 
+	import { createEventDispatcher } from 'svelte';
+
 	import Cell from './Cell.svelte';
 
 	export let column: Column;
+
+	const dispatch = createEventDispatcher();
+
+	function dispatchSortEvent() {
+		dispatch('sort', column.key);
+	}
 </script>
 
 <Cell>
-	<div class="font-semibold">
+	<div class="font-semibold cursor-pointer" on:click={dispatchSortEvent}>
 		{column.header}
 	</div>
 </Cell>
--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -8,13 +8,17 @@
 
 	export let columns: Column[];
 	export let data: DataRow[];
+
+	function handleSortEvent(event: CustomEvent) {
+		return;
+	}
 </script>
 
 <div class="flex flex-col gap-1 divide-y divid-solid p-2">
 	<Row>
 		<svelte:fragment>
 			{#each columns as column}
-				<HeaderCell {column} />
+				<HeaderCell {column} on:sort={handleSortEvent} />
 			{/each}
 		</svelte:fragment>
 	</Row>

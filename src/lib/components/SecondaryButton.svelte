83542119d49d3347d6b9d48ff248b97fbf5c5d76
Dickie Field <me@rpf3.io>
2022-08-18 19:48:53 -0400

    feat: Create primary and secondary buttons

---
+++ src/lib/components/SecondaryButton.svelte
@@ -0,0 +1,5 @@
+<button
+	class="w-full rounded-md border border-cyan-600 px-4 py-2 hover:bg-gray-50 focus:outline-none focus:ring-cyan-500 focus:ring-2 focus:ring-offset-2"
+>
+	<slot />
+</button>

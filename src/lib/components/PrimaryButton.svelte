fe201c84a0e2f0b011e27d165add6781b7833223
Dickie Field <me@rpf3.io>
2022-08-18 19:52:12 -0400

    feat: Dispatch click events from the buttons

--- src/lib/components/PrimaryButton.svelte
+++ src/lib/components/PrimaryButton.svelte
@@ -1,4 +1,15 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function dispatchEvent(event: Event) {
+		dispatch(event.type);
+	}
+</script>
+
 <button
+	on:click={dispatchEvent}
 	class="w-full rounded-md border border-transparent px-4 py-2 text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-cyan-500 focus:ring-2 focus:ring-offset-2"
 >
 	<slot />

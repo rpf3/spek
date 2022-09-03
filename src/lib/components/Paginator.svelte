1bcb8097e32ff8ed68f7caab232fd45df39c87c5
Dickie Field <me@rpf3.io>
2022-09-03 12:50:09 -0400

    feat: Add a pagination component
    This commit adds a component to control table pagination with previous
    and next buttons.

---
+++ src/lib/components/Paginator.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { page } from '$lib/stores/page';
+
+	import Left from './icons/Left.svelte';
+	import Right from './icons/Right.svelte';
+
+	function previousPage() {
+		page.set({
+			skip: $page.skip - $page.take,
+			take: $page.take
+		});
+	}
+
+	function nextPage() {
+		page.set({
+			skip: $page.skip + $page.take,
+			take: $page.take
+		});
+	}
+</script>
+
+<div class="flex justify-center gap-1">
+	<button class="border rounded p-1" on:click={previousPage}>
+		<Left />
+	</button>
+
+	<button class="border rounded p-1" on:click={nextPage}>
+		<Right />
+	</button>
+</div>

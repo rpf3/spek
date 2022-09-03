19585a11754848231f1816a71775fcc61474b82d
Dickie Field <me@rpf3.io>
2022-09-03 14:07:38 -0400

    feat: Disable the previous button when appropriate
    This commit disables the previous button of the pagination component
    when the state is at index zero.

--- src/lib/components/Paginator.svelte
+++ src/lib/components/Paginator.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import { page } from '$lib/stores/page';
+	import { onDestroy } from 'svelte';
 
 	import Left from './icons/Left.svelte';
 	import Right from './icons/Right.svelte';
+
+	let disablePrevious = false;
+
+	const unsubscribe = page.subscribe((state) => {
+		disablePrevious = state.skip === 0;
+	});
+
+	onDestroy(unsubscribe);
 </script>
 
 <div class="flex justify-center gap-1">
-	<button class="border rounded p-1" on:click={page.previous}>
+	<button class="border rounded p-1" on:click={page.previous} disabled={disablePrevious}>
 		<Left />
 	</button>
 

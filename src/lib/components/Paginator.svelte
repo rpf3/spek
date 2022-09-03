fd632f6d0d9254cb3b1c1433fabb584c75a1d653
Dickie Field <me@rpf3.io>
2022-09-03 14:02:55 -0400

    refactor: Move the pagination into the store
    This commit moves the previous and next functions into the page store.

--- src/lib/components/Paginator.svelte
+++ src/lib/components/Paginator.svelte
@@ -3,28 +3,14 @@
 
 	import Left from './icons/Left.svelte';
 	import Right from './icons/Right.svelte';
-
-	function previousPage() {
-		page.set({
-			skip: $page.skip - $page.take,
-			take: $page.take
-		});
-	}
-
-	function nextPage() {
-		page.set({
-			skip: $page.skip + $page.take,
-			take: $page.take
-		});
-	}
 </script>
 
 <div class="flex justify-center gap-1">
-	<button class="border rounded p-1" on:click={previousPage}>
+	<button class="border rounded p-1" on:click={page.previous}>
 		<Left />
 	</button>
 
-	<button class="border rounded p-1" on:click={nextPage}>
+	<button class="border rounded p-1" on:click={page.next}>
 		<Right />
 	</button>
 </div>

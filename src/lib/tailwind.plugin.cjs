d7365fb0769658d327e8f22b48d128096c6651ec
Dickie Field <me@rpf3.io>
2022-09-03 10:08:40 -0400

    feat: Add a Tailwind opacity utility function
    This commit adds a function to the Tailwind plugin that will
    allow us to define custom colors that use the Tailwind opacity
    classes.  This function was adapted from the Tailwind documentation.

--- src/lib/tailwind.plugin.cjs
+++ src/lib/tailwind.plugin.cjs
@@ -1,5 +1,15 @@
 const plugin = require('tailwindcss/plugin');
 
+function withOpacity(variableName) {
+	return ({ opacityValue }) => {
+		if (opacityValue === undefined) {
+			return `rgb(var(${variableName}))`;
+		}
+
+		return `rgba(var(${variableName}, ${opacityValue}))`;
+	};
+}
+
 module.exports = plugin(() => {}, {
 	theme: {
 		extend: {}

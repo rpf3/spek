5e151d6bd467fb41ce9d677c7f20854582c4a0b9
Dickie Field <me@rpf3.io>
2022-09-03 10:24:06 -0400

    feat: Style the button colors with CSS variables
    This commit enhances the Tailwind plugin so that the primary and
    secondary button colors are able to be themed with CSS variables.

--- src/lib/components/PrimaryButton.svelte
+++ src/lib/components/PrimaryButton.svelte
@@ -10,7 +10,7 @@
 
 <button
 	on:click={dispatchEvent}
-	class="w-full rounded-md border border-transparent px-4 py-2 text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-cyan-500 focus:ring-2 focus:ring-offset-2"
+	class="w-full rounded-md border border-transparent px-4 py-2 text-skin-inverted bg-skin-button-primary hover:opacity-75 focus:outline-none"
 >
 	<slot />
 </button>

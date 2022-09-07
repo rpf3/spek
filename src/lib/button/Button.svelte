18bb4bf1b1e31ddf03b1799e65dfa23d6ee7238d
Dickie Field <me@rpf3.io>
2022-09-07 19:48:39 -0400

    refactor: Create a single button component
    This commit combines both button components.  The type property models
    how the button is used and will control which color palette is applied.
    The mode property controls if the button is filled or ringed.  I've also
    taken the opportunity to rename the theming variables to line up with
    the examples given by Tailwind themselves in terms of primary and
    accent vs primary and secondary.

---
+++ src/lib/button/Button.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { ButtonType, ButtonMode } from './types';
+	import { createEventDispatcher } from 'svelte';
+
+	export let type: ButtonType;
+	export let mode: ButtonMode;
+
+	const dispatch = createEventDispatcher();
+
+	function dispatchEvent(event: Event) {
+		dispatch(event.type);
+	}
+</script>
+
+<button
+	on:click={dispatchEvent}
+	class="w-full rounded-md border px-4 py-2 focus:outline-none"
+	class:bg-transparent={mode === ButtonMode.Ring}
+	class:bg-skin-button-primary={mode === ButtonMode.Fill && type === ButtonType.Primary}
+	class:bg-skin-button-accent={mode === ButtonMode.Fill && type === ButtonType.Accent}
+	class:border-transparent={mode === ButtonMode.Fill}
+	class:border-skin-button-primary={mode === ButtonMode.Ring && type === ButtonType.Primary}
+	class:border-skin-button-accent={mode === ButtonMode.Ring && type === ButtonType.Accent}
+	class:text-skin-inverted={mode === ButtonMode.Fill}
+	class:hover:opacity-75={mode === ButtonMode.Fill}
+	class:hover:bg-skin-button-ring-hover={mode === ButtonMode.Ring}
+>
+	<slot />
+</button>

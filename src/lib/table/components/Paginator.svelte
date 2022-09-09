8abbd678ff1aae420f4bb823091a3f23ab3f57c4
Dickie Field <me@rpf3.io>
2022-09-09 18:02:21 -0400

    feat: Add smarter pagination logic
    This commit enhances the pagination component so that the first and last
    few pages are always visible as well as the pages surrounding the
    current page.  The remaining gaps are filled by ellipses.

--- src/lib/table/components/Paginator.svelte
+++ src/lib/table/components/Paginator.svelte
@@ -6,16 +6,91 @@
 	import Left from '$lib/icon/Left.svelte';
 	import Right from '$lib/icon/Right.svelte';
 
+	const maxPageCount = 9;
+
 	let disablePrevious = false;
 	let disableNext = false;
 	let pageCount = Math.ceil($repository.total / $page.take);
 	let pageNumber = $page.skip / $page.take;
+
+	let visiblePageNumbers: number[];
+
+	function getVisiblePageNumbers(): number[] {
+		if (pageCount <= maxPageCount) {
+			return [...Array(pageCount).keys()];
+		}
+
+		const pageNumbers = new Set<number>();
+
+		// Display the first and last two pages.
+		pageNumbers.add(0);
+		pageNumbers.add(1);
+
+		pageNumbers.add(pageCount - 2);
+		pageNumbers.add(pageCount - 1);
+
+		// Display the surrounding two pages.
+		pageNumbers.add(pageNumber - 1);
+		pageNumbers.add(pageNumber);
+		pageNumbers.add(pageNumber + 1);
+
+		// Pad out the first few pages
+		if (pageNumber < 3) {
+			pageNumbers.add(2);
+			pageNumbers.add(3);
+			pageNumbers.add(4);
+		}
+
+		// Pad out the last few pages.
+		if (pageNumber > pageCount - 4) {
+			pageNumbers.add(pageCount - 5);
+			pageNumbers.add(pageCount - 4);
+			pageNumbers.add(pageCount - 3);
+		}
+
+		return Array.from(pageNumbers);
+	}
+
+	function truncatePageNumbers(pageNumbers: number[]): number[] {
+		const truncatedPageNumbers: number[] = [];
+
+		let n = 0;
+
+		for (var i = 0; i < pageNumbers.length; i++) {
+			const m = pageNumbers[i];
+
+			if (n !== m) {
+				truncatedPageNumbers.push(NaN);
+				n = m;
+			}
+
+			truncatedPageNumbers.push(m);
+			n++;
+		}
+
+		return truncatedPageNumbers;
+	}
+
+	function getPageNumbers(): number[] {
+		const pageNumbers = getVisiblePageNumbers();
+
+		const validPageNumbers = pageNumbers.filter((n) => {
+			return n > -1 && n < pageCount;
+		});
+
+		const orderedPageNumbers = validPageNumbers.sort((n, m) => n - m);
+
+		const truncatedPageNumbers = truncatePageNumbers(orderedPageNumbers);
+
+		return truncatedPageNumbers;
+	}
 
 	const unsubscribe = repository.subscribe((data) => {
 		disablePrevious = $page.skip === 0;
 		disableNext = $page.skip + $page.take >= data.total;
 		pageCount = Math.ceil(data.total / $page.take);
 		pageNumber = $page.skip / $page.take;
+		visiblePageNumbers = getPageNumbers();
 	});
 
 	onDestroy(unsubscribe);
@@ -30,13 +105,14 @@
 		<Left />
 	</button>
 
-	{#each Array(pageCount) as _, i}
+	{#each visiblePageNumbers as n}
 		<button
 			class="p-1 hover:bg-skin-button-ring-hover disabled:hover:bg-inherit"
-			class:bg-skin-button-ring-hover={i === pageNumber}
-			on:click={() => page.set(i)}
+			class:bg-skin-button-ring-hover={n === pageNumber}
+			disabled={isNaN(n)}
+			on:click={() => page.set(n)}
 		>
-			<div class="px-2">{i + 1}</div>
+			<div class="px-2">{isNaN(n) ? '…' : n + 1}</div>
 		</button>
 	{/each}
 

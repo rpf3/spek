71720c87c540ab85dfc4d740870445d959369a78
Dickie Field <me@rpf3.io>
2022-09-13 19:51:20 -0400

    fix: Apply the facade background to filter inputs

--- src/lib/table/components/Filter.svelte
+++ src/lib/table/components/Filter.svelte
@@ -63,7 +63,10 @@
 		<p slot="header">Filter</p>
 
 		<div slot="body" class="grid grid-cols-2 gap-5">
-			<select bind:value={selectedFilterKey} class="border border-slate-200 rounded px-1 py-1">
+			<select
+				bind:value={selectedFilterKey}
+				class="border border-slate-200 rounded px-1 py-1 dark:bg-spek-facade"
+			>
 				{#each filterableColumns as column}
 					<option value={column.key}>{column.header}</option>
 				{/each}
@@ -71,7 +74,7 @@
 
 			<input
 				type="text"
-				class="border border-slate-200 rounded px-2 py-1"
+				class="border border-slate-200 rounded px-2 py-1 dark:bg-spek-facade"
 				bind:value={selectedFilterValue}
 			/>
 		</div>

5860e15ec4d32a13a14a2cd76a2b22b812798bf2
Dickie Field <me@rpf3.io>
2022-09-17 08:43:38 -0400

    feat: Add a menu component
    This commit adds a new component for popout menus.

---
+++ src/lib/menu/Menu.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	let visible = false;
+	let menu: HTMLElement;
+
+	function handleClick(event: Event) {
+		if (!visible) {
+			return;
+		}
+
+		const target = event.target as HTMLElement;
+
+		if (!menu.contains(target)) {
+			visible = false;
+		}
+	}
+
+	function handleToggle() {
+		visible = !visible;
+	}
+</script>
+
+<svelte:window on:click={handleClick} />
+
+<div class="relative" bind:this={menu}>
+	<div class="flex" on:click={handleToggle}>
+		<slot name="toggle" />
+	</div>
+
+	<div
+		class="absolute top-full right-0 bg-spek-facade border rounded shadow shadow-white p-1"
+		class:hidden={visible === false}
+		class:block={visible}
+	>
+		<slot name="content" />
+	</div>
+</div>

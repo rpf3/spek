c497e7edf0f42fc2f5f2a1b152e61ce77cf86d9d
Dickie Field <me@rpf3.io>
2022-09-17 09:22:28 -0400

    feat: Refactor the filter component
    This commit adds a new component that utilizes both the chip and menu
    components to hopefully enhance how the table data is filtered.
    Clicking the filter icon now opens a menu of all the filterable column
    headers.  Selecting one of these adds a chip which also has a menu you
    can expand that allows users to enter a filter value.

--- src/lib/table/components/Filter.svelte
+++ src/lib/table/components/Filter.svelte
@@ -4,54 +4,19 @@
 	import { filter } from '$lib/table/stores/filter';
 	import { page } from '$lib/table/stores/page';
 	import { onDestroy } from 'svelte';
-	import { ButtonType, ButtonMode } from '$lib/button/types';
 
 	import Filter from '$lib/icon/Filter.svelte';
-	import Dialog from '$lib/dialog/Dialog.svelte';
-	import Button from '$lib/button/Button.svelte';
-	import Close from '$lib/icon/Close.svelte';
-	import Chip from '$lib/chip/Chip.svelte';
+	import Menu from '$lib/menu/Menu.svelte';
+	import FilterChip from '$lib/table/components/FilterChip.svelte';
 
 	export let columns: Column[];
 
 	let filterableColumns = columns.filter((column) => column.filterable);
 
-	let isModalVisible = false;
+	let potentialFilters: Column[] = [];
 
-	let selectedFilterKey: string;
-	let selectedFilterValue: string;
-
-	function openFilterModal() {
-		isModalVisible = true;
-	}
-
-	function closeFilterModal() {
-		isModalVisible = false;
-
-		selectedFilterKey = filterableColumns.at(0)?.key || '';
-		selectedFilterValue = '';
-	}
-
-	function applyFilter() {
-		filter.add(selectedFilterKey, selectedFilterValue);
-
-		closeFilterModal();
-	}
-
-	function clearFilter() {
-		filter.clear();
-
-		closeFilterModal();
-	}
-
-	function removeFilter(key: string) {
-		filter.remove(key);
-	}
-
-	function getColumnHeader(key: string): string | undefined {
-		const column = filterableColumns.find((col) => col.key === key);
-
-		return column?.header;
+	function addFilterChip(column: Column) {
+		potentialFilters = [...potentialFilters, column];
 	}
 
 	const unsubscribe = filter.subscribe(() => {
@@ -61,52 +26,28 @@
 	onDestroy(unsubscribe);
 </script>
 
-{#if $filter.filters.length > 0}
+{#if potentialFilters.length > 0}
 	<div class="inline mr-3">
 		<div class="flex gap-3">
-			{#each $filter.filters as filter}
-				<Chip>
-					<span class="font-semibold">{getColumnHeader(filter.key)}:</span>
-					<span>{filter.value}</span>
-					<button on:click={() => removeFilter(filter.key)}>
-						<Close />
-					</button>
-				</Chip>
+			{#each potentialFilters as column}
+				<FilterChip {column} />
 			{/each}
 		</div>
 	</div>
 {/if}
 
 {#if filterableColumns.length > 0}
-	<button on:click={openFilterModal}>
-		<Filter />
-	</button>
-{/if}
+	<Menu>
+		<button slot="toggle">
+			<Filter />
+		</button>
 
-{#if isModalVisible}
-	<Dialog on:close={closeFilterModal}>
-		<p slot="header">Filter</p>
-
-		<div slot="body" class="grid grid-cols-2 gap-5">
-			<select
-				bind:value={selectedFilterKey}
-				class="border rounded px-1 py-1 focus:outline-none dark:bg-spek-facade"
-			>
-				{#each filterableColumns as column}
-					<option value={column.key}>{column.header}</option>
-				{/each}
-			</select>
-
-			<input
-				type="text"
-				class="border rounded px-2 py-1 focus:outline-none dark:bg-spek-facade"
-				bind:value={selectedFilterValue}
-			/>
-		</div>
-
-		<div slot="footer" class="flex flex-row gap-3">
-			<Button type={ButtonType.Primary} mode={ButtonMode.Ring} on:click={clearFilter}>Clear</Button>
-			<Button type={ButtonType.Primary} mode={ButtonMode.Fill} on:click={applyFilter}>Apply</Button>
-		</div>
-	</Dialog>
+		<ul slot="content" class="w-48">
+			{#each filterableColumns as column}
+				<li class="px-3 my-1 cursor-pointer" on:click={() => addFilterChip(column)}>
+					{column.header}
+				</li>
+			{/each}
+		</ul>
+	</Menu>
 {/if}
---
+++ src/lib/table/components/FilterChip.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { Column } from '$lib/table/types';
+
+	import { filter } from '$lib/table/stores/filter';
+
+	import Chip from '$lib/chip/Chip.svelte';
+	import Menu from '$lib/menu/Menu.svelte';
+	import Down from '$lib/icon/Down.svelte';
+	import Trash from '$lib/icon/Trash.svelte';
+
+	export let column: Column;
+
+	let value: string;
+
+	function handleChange() {
+		filter.add(column.key, value);
+	}
+</script>
+
+<Chip>
+	<span class="font-semibold">{column.header}</span>
+
+	<Menu>
+		<button slot="toggle">
+			<Down />
+		</button>
+
+		<div slot="content" class="flex gap-3 p-1">
+			<button>
+				<Trash />
+			</button>
+
+			<input
+				type="text"
+				bind:value
+				on:change={handleChange}
+				class="text-sm bg-spek-facade border rounded w-36 p-1"
+			/>
+		</div>
+	</Menu>
+</Chip>

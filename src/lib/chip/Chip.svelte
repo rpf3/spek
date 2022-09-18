3336121cc01eebdf8135d3f2d02e5d78c1233860
Dickie Field <me@rpf3.io>
2022-09-18 16:40:02 -0400

    fix: Invert the chip text color

--- src/lib/chip/Chip.svelte
+++ src/lib/chip/Chip.svelte
@@ -1,3 +1,5 @@
-<div class="flex gap-1 items-center rounded-xl text-sm px-3 py-1 bg-spek-primary-600">
+<div
+	class="flex gap-1 items-center rounded-xl text-sm px-3 py-1 bg-spek-primary-600 text-spek-inverted"
+>
 	<slot />
 </div>

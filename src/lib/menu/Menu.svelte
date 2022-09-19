4bb5a3b0450b802ed3f93f0d86163bc702d6e10e
Dickie Field <me@rpf3.io>
2022-09-19 09:04:05 -0400

    refactor: Remove the menu border in dark mode

--- src/lib/menu/Menu.svelte
+++ src/lib/menu/Menu.svelte
@@ -28,7 +28,7 @@
 	</div>
 
 	<div
-		class="absolute top-full right-0 mt-1 bg-spek-facade-50 dark:bg-spek-facade-600 border rounded shadow-lg shadow-spek-facade-400 dark:shadow-none p-1"
+		class="absolute top-full right-0 mt-1 bg-spek-facade-50 dark:bg-spek-facade-600 border dark:border-transparent rounded shadow-lg shadow-spek-facade-400 dark:shadow-none p-1"
 		class:hidden={visible === false}
 		class:block={visible}
 	>

ad7d76bb09fb1675bc15f097f34a0ccc308063f2
Dickie Field <me@rpf3.io>
2022-09-19 19:39:16 -0400

    feat: Add support for cell date values

--- src/lib/table/utils/index.ts
+++ src/lib/table/utils/index.ts
@@ -1,6 +1,6 @@
 import type { DataRow } from '$lib/table/types';
 
-function getCellValue(row: DataRow, key: string): string | number | null {
+function getCellValue(row: DataRow, key: string): string | number | Date | null {
 	const dataItem = row.items.find((item) => item.key === key);
 
 	if (dataItem === undefined) {

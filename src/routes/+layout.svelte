7a081754f1794d6bef11c14739da4c8fe7970bec
Richard P. Field III <me@rpf3.io>
2023-07-03 17:45:21 -0400

    fix: Add bottom padding to the test page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="min-h-screen bg-zinc-50 dark:bg-zinc-700 dark:text-white">
-	<div class="max-w-screen-lg mx-auto pt-8">
+	<div class="max-w-screen-lg mx-auto py-8">
 		<slot />
 	</div>
 </div>

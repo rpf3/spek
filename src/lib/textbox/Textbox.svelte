fb7dee97b9a6a56a4d63f0fe1abfb28517e06723
Richard P. Field III <me@rpf3.io>
2024-05-27 11:31:25 -0400

    refactor: Propagate native component events
    This commit propagates the native component events like click instead
    of intercepting them with Svelte and re-dispatching them.

--- src/lib/textbox/Textbox.svelte
+++ src/lib/textbox/Textbox.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-
 	export let value: string;
 	export let name = '';
-
-	const dispatch = createEventDispatcher();
-
-	function dispatchEvent(event: Event) {
-		dispatch(event.type);
-	}
 </script>
 
 <input
 	{name}
 	type="text"
 	bind:value
-	on:change={dispatchEvent}
+	on:change
 	class="w-full rounded border bg-spek-facade-50 p-1 text-sm outline-none outline-offset-0 focus:outline-spek-primary-400 dark:border-transparent dark:bg-spek-facade-500"
 />
